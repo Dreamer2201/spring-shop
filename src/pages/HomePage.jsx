d6d506d22424052afbd9f0c1d30d86e0b6096350
Oksana Polyakova <ddreamer22011986@gmail.com>
2023-02-26 18:56:55 +0200

    swiper js on Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,16 +2,65 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import { useAuth } from '../hooks/useAuth'
 import { removeUser } from '../store/slises/userSlise';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import img1 from '../images-house/1.jpg'
+import img2 from '../images-house/2.jpg'
+import img3 from '../images-house/3.png'
+import img4 from '../images-house/4.jpg'
+import img5 from '../images-house/5.png'
+import { StyledBoxSlider, ThumbImg, StyledImg, BtnNext, BtnPrev } from './StylesBgSlider';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default function HomePage() {
   const dispatch = useDispatch();
   const {isAuth, email} = useAuth();
   console.log(isAuth);
-  console.log(email)
+  console.log(email);
+
   return (
     <div>
       {isAuth ? <p>Wellcome, {email}</p> : <p>Wellcome, dear guest</p>}
         <h1>Home Page about shop</h1>
+        <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false
+      }}
+      spaceBetween={10}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <ThumbImg>
+          <StyledImg src={img1} alt="bg"/>
+        </ThumbImg>
+      </SwiperSlide>
+      <SwiperSlide>
+        <ThumbImg>
+          <StyledImg src={img2} alt="bg"/>
+        </ThumbImg>
+      </SwiperSlide>
+      <SwiperSlide>
+        <ThumbImg>
+          <StyledImg src={img3} alt="bg"/>
+        </ThumbImg>
+      </SwiperSlide>
+      <SwiperSlide>
+        <ThumbImg>
+          <StyledImg src={img4} alt="bg"/>
+        </ThumbImg>
+      </SwiperSlide>
+    </Swiper>
         {isAuth ? <button type='button' onClick={() => dispatch(removeUser())}>Log out</button> : <></>}
     </div>
   )
---
+++ src/pages/StylesBgSlider.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+const StyledBoxSlider = styled.div`
+    position: relative;
+    margin-top: 50px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    height: 350px;
+    outline: 1px solid black;
+    overflow: hidden;
+`;
+const ThumbImg = styled.div`
+    width: 100%;
+    height: 350px;
+`
+
+const StyledImg = styled.img`
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+const BtnPrev = styled.button`
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    background-color: green;
+    z-index: 50;
+    position: absolute;
+    top: 50%;
+    left: 10px;
+    transform: translate(0, -50%);
+`
+
+const BtnNext = styled.button`
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    background-color: green;
+    z-index: 50;
+    position: absolute;
+    top: 50%;
+    right: 10px;
+    transform: translate(0, -50%);
+`
+//   #prevImgSecond {
+//     position: absolute;
+//     left: 20px;
+//     top: 50%;
+//     z-index: 50;
+//   }
+//   #nextImgSecond {
+//     position: absolute;
+//     right: 20px;
+//     top: 50%;
+//     z-index: 50;
+//   }
+
+export {StyledBoxSlider, ThumbImg, StyledImg, BtnNext, BtnPrev}
